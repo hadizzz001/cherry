8ad050cd00d388239503c9c3dedf1e6cbda93a17
Your Name <you@example.com>
2023-07-10 12:28:00 +0300

    Add existing file

--- utils/index.ts
+++ utils/index.ts
@@ -2,7 +2,7 @@
 
 export async function fetchTemp() { 
 
-const response = await fetch('http://localhost:3000/api/blog' );
+const response = await fetch('https://enchanting-dusk-e6cd04.netlify.app/api/blog' );
 
     const result = await response.json();
 
@@ -11,7 +11,8 @@ const response = await fetch('http://localhost:3000/api/blog' );
 
 export async function fetchTemp1() { 
 
-const response = await fetch('http://localhost:3000/api/blogs' );
+const response = await fetch('https://enchanting-dusk-e6cd04.netlify.app/api/blogs' );
+// const response = await fetch('http://localhost:3000/api/blogs' );
 
     const result = await response.json();
 
